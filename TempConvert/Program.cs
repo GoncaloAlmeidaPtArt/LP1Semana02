19816a0bd86b05bd91e3fae918abe9425bd0bdf2
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-02-26 16:42:22 +0000

    Adição de comentarios

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -7,11 +7,14 @@ namespace TempConvert
     {
         private static void Main(string[] args)
         {
+            //A pedido do professor
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
+            //Peço a temperatura
             Console.Write("Temperature: ");
             string temperatura = Console.ReadLine();
 
+            //Peço o tipo de temperatura
             Console.Write("Unit (C/F): ");
             string unidade = Console.ReadLine();
 
@@ -19,6 +22,8 @@ namespace TempConvert
             {
                 case "C":
 
+                    //mostra como double a temperatura escolhida, a versão oposta 
+                    //da escolhida e o Absolute da escolhida 
                     double temperaturaFC = Convert.ToDouble(temperatura);
                     Console.WriteLine($"{temperaturaFC:f2} C = {temperaturaFC * 1.8 + 32:f2} F");
                     Console.WriteLine($"Absolute Value: {Math.Abs(temperaturaFC * 1.8 + 32):f2}");
@@ -27,6 +32,8 @@ namespace TempConvert
 
                 case "F":
 
+                    //mostra como double a temperatura escolhida, a versão oposta 
+                    //da escolhida e o Absolute da escolhida 
                     double temperaturaFF = Convert.ToDouble(temperatura);
                     Console.WriteLine($"{temperaturaFF:f2} F = {(temperaturaFF - 32)/1.8:f2} C");
                     Console.WriteLine($"Absolute Value: {Math.Abs((temperaturaFF - 32)/1.8):f2}");
@@ -34,7 +41,8 @@ namespace TempConvert
                     break;
 
                 default:
-
+                    
+                    //Indica que o utilizador não escolheu nenhuma das opções
                     Console.WriteLine("Invalid unit.");
 
                     break;

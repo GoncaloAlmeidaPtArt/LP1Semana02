b77e4e799f048996aba632038b2acf86ea2d7a8c
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-02-26 16:44:25 +0000

    Alteração do texto mostrado ao utilizador

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -26,7 +26,7 @@ namespace TempConvert
                     //da escolhida e o Absolute da escolhida 
                     double temperaturaFC = Convert.ToDouble(temperatura);
                     Console.WriteLine($"{temperaturaFC:f2} C = {temperaturaFC * 1.8 + 32:f2} F");
-                    Console.WriteLine($"Absolute Value: {Math.Abs(temperaturaFC * 1.8 + 32):f2}");
+                    Console.WriteLine($"Absolute value: {Math.Abs(temperaturaFC * 1.8 + 32):f2}");
 
                     break;
 
@@ -36,7 +36,7 @@ namespace TempConvert
                     //da escolhida e o Absolute da escolhida 
                     double temperaturaFF = Convert.ToDouble(temperatura);
                     Console.WriteLine($"{temperaturaFF:f2} F = {(temperaturaFF - 32)/1.8:f2} C");
-                    Console.WriteLine($"Absolute Value: {Math.Abs((temperaturaFF - 32)/1.8):f2}");
+                    Console.WriteLine($"Absolute value: {Math.Abs((temperaturaFF - 32)/1.8):f2}");
 
                     break;
 

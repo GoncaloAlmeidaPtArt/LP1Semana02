80d3d7e6dee91f69f2b2a8556dfefbfa7cdfbbe9
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-02-26 16:06:01 +0000

    Criado o projeto

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string textouser = Console.ReadLine();
+
+            int numerouser = Convert.ToInt32(textouser);
+
+            sbyte n = Convert.ToSByte(numerouser);
+
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+        }
+    }
+}
---
+++ TempConvert/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace TempConvert
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
